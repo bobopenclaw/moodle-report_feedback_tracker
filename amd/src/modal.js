54ca6dc0fc745bc72057448da52e0ce14cbc9e0b
Matthias Opitz <opitz@gmx.de>
2024-11-25 19:01:01 +0000

    CTP-4089 fix modal (#48)
    * CTP-3956 : course level report
    - saving assessment type while preventing locked ones from editing
    - introducing supported_module()
    - For assessment types other than DUMMY 'Hidden from student report' is disabled by default

--- amd/src/modal.js
+++ amd/src/modal.js
@@ -4,7 +4,6 @@ export const init = async() => {
     const feedbackDuedatePicker = document.getElementById('feedbackduedate');
     const hidingCheckbox = document.getElementById('hidden');
     const previousFeedbackDuedate = document.getElementById('previousfeedbackduedate');
-    const reason = document.getElementById('js-reason');
 
     if (assessmentTypeSelector) {
         assessmentTypeSelector.addEventListener('change', async(e) => {
@@ -12,21 +11,26 @@ export const init = async() => {
             // TODO: Use assess_type::ASSESS_TYPE_DUMMY and assess_type::ASSESS_TYPE_SUMMATIVE.
             const assessTypeDummy = 2;
 
-            if (assessmentType * 1 === assessTypeDummy) {
-                hidingCheckbox.checked = true; // Dummy assessments are hidden from students.
-                hidingCheckbox.disabled = true;
+            if (parseInt(assessmentType, 10) === assessTypeDummy) {
+                hidingCheckbox.checked = true; // Dummy assessments are hidden from the student report.
+                hidingCheckbox.disabled = true; // Changing hiding state is disabled.
             } else {
-                hidingCheckbox.disabled = false;
+                hidingCheckbox.checked = false; // Other assessments are shown to students by default.
+                hidingCheckbox.disabled = false; // Changing hiding state is enabled.
             }
         });
     }
 
     if (feedbackDuedatePicker) {
         feedbackDuedatePicker.addEventListener('change', function() {
+            const reason = document.getElementById('js-reason');
+            const reasonField = document.getElementById('reason');
             if (feedbackDuedatePicker.value === previousFeedbackDuedate.value || !feedbackDuedatePicker.value) {
-                reason.classList.add('d-none'); // Hide the input field for a reason.
+                reason.classList.add('d-none'); // Hide the input field.
+                reasonField.required = false;
             } else {
-                reason.classList.remove('d-none'); // Show an input field for a reason.
+                reason.classList.remove('d-none'); // Show an input field.
+                reasonField.required = true; // Make it a required field.
             }
         });
     }
