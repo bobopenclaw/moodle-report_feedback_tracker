057c47abe4576d5fa9f02aa7c4a7eeb33b281e1a
Alistair Spark <aspark21@users.noreply.github.com>
2025-02-04 13:18:43 +0000

    Version bump for site report

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2025011400;                    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025020400;                    // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024042200;                    // Requires this Moodle version.
 $plugin->component = 'report_feedback_tracker';     // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [
